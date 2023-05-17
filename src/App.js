3121a86f5ff854dbf96bc73a817c60f701f5e8ca
filippennsater <filip.pennsater@hotmail.com>
2023-05-17 19:02:44 +0200

    Fixed overall file-structure for project

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+
+import React from 'react';
+import Navbar from 'components/navbar/Navbar';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import Home from '/pages/home/Home';
+import Market from '/pages/market/Market';
+import Favorites from '/pages/favorites/Favorites';
+import About from '/pages/about/About';
 
 function App() {
   return (
     
     <>
-    
+    <Router>
+        <Navbar/>
+        <Routes>
+          <Route path="/" exact Component={Home} />
+          <Route path="/market" exact Component={Market} />
+          <Route path="/favorites" exact Component={Favorites} />
+          <Route path="/about" exact Component={About} />
+        </Routes>
+      </Router>
     </>
 
   );
