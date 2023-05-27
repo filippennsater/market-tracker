651ab5fdc8195b3d50fcc09a31cce9941ca4c740
filippennsater <filip.pennsater@hotmail.com>
2023-05-27 20:47:15 +0200

    added some footers

--- src/pages/Favorites/Favorites.jsx
+++ src/pages/Favorites/Favorites.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import '../../App.css'
 import FavoriteSection from './FavoriteSection'
+import Footer from '../../components/footer/Footer';
 
 function Favorites (){
     return (
     <>
       <FavoriteSection/>
+      <Footer/>
     </>
     );
 }
--- src/pages/Market/Market.jsx
+++ src/pages/Market/Market.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import '../../App.css'
 import MarketSection from './MarketSection'
+import Footer from '../../components/footer/Footer';
 
 function Market (){
     return (
     <>
       <MarketSection/>
+      <Footer/>
     </>
     );
 }
