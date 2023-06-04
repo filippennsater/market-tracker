3aa56f3685995487fa15af2a8b1cd84ef4d4b417
filippennsater <filip.pennsater@hotmail.com>
2023-06-04 21:36:10 +0200

    Started implementing Shares

---
+++ src/components/sharesETC/SharesAndCurrencies.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './SharesAndCurrencies.scss';
+
+function Shares(props) {
+    return (
+        <>
+            <li className='cards__item'>
+
+                <Link className='cards__item__link' to={props.path} target="_blank">
+                    <figure className='cards__item__pic-wrap' data-category={props.label}> 
+                        <img 
+                        src={props.src} 
+                        alt='ProjectImage' 
+                        className='cards__item__img' 
+                        />
+                    </figure>
+                    <div className='cards__item__info'>
+                        <h10 className='cards__item__text1'>
+                            {props.text}
+                        </h10>
+                    </div>
+                </Link>
+            </li>
+        </>
+    );
+}
+
+export default Shares;
