9809a5065aab08723e95dbed4695d27610caf9f1
filippennsater <filip.pennsater@hotmail.com>
2023-06-05 23:13:07 +0200

    Added some styling for shares and inserted in homepage

--- src/components/sharesETC/Shares.jsx
+++ src/components/sharesETC/Shares.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import './SharesAndCurrencies.scss';
+import './Shares.scss';
 
 function Shares(props) {
     return (
--- src/pages/Home/Homesection.jsx
+++ src/pages/Home/Homesection.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import '../../App.css';
 import './HomeSection.scss';
+import Shares from '../../components/sharesETC/Shares';
+
 
 function HomeSection() {
     return (
@@ -10,6 +12,32 @@ function HomeSection() {
             <p>Home</p>
 
             <div className='previewContainer'>
+
+
+
+
+            <div className='cards__container'>
+                <div className='cards__wrapper'>
+
+                <ul className='cards__items'>
+                        <Shares src='images/img-portfolio1.png' text='My Portfolio' label='Bigger Project' path='/' />
+                        <Shares src='images/img-GoodGains.png' text='GoodGains' label='Bigger Project' path='https://goodgains-1202d.web.app' />
+                        <Shares src='images/loading.gif' text='MarketTracker (Coming soon)' label='Bigger Project' path='/projects' />
+                    </ul>
+
+                    <ul className='cards__items'>
+                        <Shares src='images/img-8ball.png' text='Magic 8-ball' label='Small project' path='https://magic8ball-50df8.web.app/' />
+                        <Shares src='images/img-Apiano.png' text='The Amazing Piano' label='Small project' path='https://theamazingpiano-65a1c.web.app/' />
+                    </ul>
+
+
+                </div>
+            </div>        
+
+
+
+
+
                 
             </div>
 
