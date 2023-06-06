b129b6d89ff64930a12d4c65b4029e518f98c47d
filippennsater <filip.pennsater@hotmail.com>
2023-06-06 21:36:57 +0200

    Styled the navbar

--- src/pages/Home/Homesection.jsx
+++ src/pages/Home/Homesection.jsx
@@ -23,13 +23,8 @@ function HomeSection() {
                         <Shares src='images/img-portfolio1.png' text='My Portfolio' label='Bigger Project' path='/' />
                         <Shares src='images/img-GoodGains.png' text='GoodGains' label='Bigger Project' path='https://goodgains-1202d.web.app' />
                         <Shares src='images/loading.gif' text='MarketTracker (Coming soon)' label='Bigger Project' path='/projects' />
-                    </ul>
-
-                    <ul className='cards__items'>
                         <Shares src='images/img-8ball.png' text='Magic 8-ball' label='Small project' path='https://magic8ball-50df8.web.app/' />
-                        <Shares src='images/img-Apiano.png' text='The Amazing Piano' label='Small project' path='https://theamazingpiano-65a1c.web.app/' />
                     </ul>
-
 
                 </div>
             </div>        
