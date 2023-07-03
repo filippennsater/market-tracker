f242aac0c96c20ef9f53d73847b439ee2db1d6f3
filippennsater <filip.pennsater@hotmail.com>
2023-07-03 23:38:01 +0200

    adding list

--- src/pages/Home/Homesection.jsx
+++ src/pages/Home/Homesection.jsx
@@ -13,14 +13,14 @@ function HomeSection() {
 
             <div className='previewContainer'>
 
-
+                <ul className='share-list'>
 
                             <Shares src='images/img-portfolio1.png' text='My Portfolio' label='Bigger Project' path='/' />
                             <Shares src='images/img-GoodGains.png' text='GoodGains' label='Bigger Project' path='https://goodgains-1202d.web.app' />
                             <Shares src='images/loading.gif' text='MarketTracker (Coming soon)' label='Bigger Project' path='/projects' />
                             <Shares src='images/img-8ball.png' text='Magic 8-ball' label='Small project' path='https://magic8ball-50df8.web.app/' />
                         
-
+                </ul>
 
             </div>
 
