bb409ce14b2370d6f2c13130dcebffcebd5b78e4
filippennsater <filip.pennsater@hotmail.com>
2023-07-04 01:05:42 +0200

    fixing styling for list in homesection

--- src/pages/Home/Homesection.jsx
+++ src/pages/Home/Homesection.jsx
@@ -17,7 +17,7 @@ function HomeSection() {
 
                             <Shares src='images/img-portfolio1.png' text='My Portfolio' label='Bigger Project' path='/' />
                             <Shares src='images/img-GoodGains.png' text='GoodGains' label='Bigger Project' path='https://goodgains-1202d.web.app' />
-                            <Shares src='images/loading.gif' text='MarketTracker (Coming soon)' label='Bigger Project' path='/projects' />
+                            <Shares src='images/loading.gif' text='MarketTrac' label='Bigger Project' path='/projects' />
                             <Shares src='images/img-8ball.png' text='Magic 8-ball' label='Small project' path='https://magic8ball-50df8.web.app/' />
                         
                 </ul>
