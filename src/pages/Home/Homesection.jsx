7d4840409a2c2c6587626769bf9904b54bb8190c
filippennsater <filip.pennsater@hotmail.com>
2023-07-09 20:51:26 +0200

    fixing styling

--- src/pages/Home/Homesection.jsx
+++ src/pages/Home/Homesection.jsx
@@ -33,6 +33,18 @@ function HomeSection() {
                 <Shares src='images/img-.png' text='Magic' label='Small project' path='' />
                 <Shares src='images/img-.png' text='Magic' label='Small project' path='' />
                 <Shares src='images/img-.png' text='Magic' label='Small project' path='' />
+                <Shares src='images/img-.png' text='Magic' label='Small project' path='' />
+                <Shares src='images/img-.png' text='Magic' label='Small project' path='' />
+                <Shares src='images/img-.png' text='Magic' label='Small project' path='' />
+                <Shares src='images/img-.png' text='Magic' label='Small project' path='' />
+                <Shares src='images/img-.png' text='Magic' label='Small project' path='' />
+                <Shares src='images/img-.png' text='Magic' label='Small project' path='' />
+                <Shares src='images/img-.png' text='Magic' label='Small project' path='' />
+                <Shares src='images/img-.png' text='Magic' label='Small project' path='' />
+                <Shares src='images/img-.png' text='Magic' label='Small project' path='' />
+                <Shares src='images/img-.png' text='Magic' label='Small project' path='' />
+                <Shares src='images/img-.png' text='Magic' label='Small project' path='' />
+                <Shares src='images/img-.png' text='Magic' label='Small project' path='' />
 
                 </ul>
 
