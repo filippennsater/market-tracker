2efd01ded178b2533d006196120e4effe39d6d66
filippennsater <filip.pennsater@hotmail.com>
2023-07-17 23:10:08 +0200

    testing

--- src/components/sharesETC/Shares.jsx
+++ src/components/sharesETC/Shares.jsx
@@ -8,11 +8,11 @@ function Shares(props) {
             <li className='share'>
 
                 <Link className='share__link' to={props.path} target="_blank">
-                    <figure className='share__pic-wrap' data-category={props.label}> 
-                        <img 
-                        src={props.src} 
-                        alt='ProjectImage' 
-                        className='share__img' 
+                    <figure className='share__pic-wrap' data-category={props.label}>
+                        <img
+                            src={props.src}
+                            alt='ProjectImage'
+                            className='share__img'
                         />
                     </figure>
                     <div className='share__info'>
